55d8884c6570b39e97dadf943e25400a6c7bc934
Ji99 <rksk0531@naver.com>
2022-07-01 21:47:30 +0900

    sdf

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("render", counter);
-  return (
-    <div>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {}, []);
+  console.log(movies);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
