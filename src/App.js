873b9ab49a578bb16d33189b25a04e375292ef88
Ji99 <rksk0531@naver.com>
2022-07-02 14:44:33 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 
 function App() {
-<<<<<<< HEAD
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
@@ -15,39 +14,6 @@ function App() {
   useEffect(() => {}, []);
   console.log(movies);
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
-=======
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
-  };
-  return (
-    <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Wirte yout to do.."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
->>>>>>> b91108696176679fcd122338033a04abbd0bc955
 }
 
 export default App;
