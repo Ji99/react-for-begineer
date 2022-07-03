36bdbf1c1364b15f5055fd95c37dc63b83efc120
Ji99 <rksk0531@naver.com>
2022-07-03 15:40:59 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import { useState, useEffect } from "react";
+import Movie from "./Movie";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
-    );
-    const json = await response.json();
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+      )
+    ).json();
+    console.log(json);
     setMovies(json.data.movies);
     setLoading(false);
   };
-  useEffect(() => {}, []);
-  console.log(movies);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie coverImg={movie.medium_cover_image} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
